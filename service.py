452119f7490073242536282a1b4b7bfd3e5cf0ef
Sergey Kolosov <K139139@yandex.ru>
2024-01-10 13:28:03 +0300

    Добавлено логирование и работа с исключениями.

--- service.py
+++ service.py
@@ -2,23 +2,31 @@ import redis
 from clickhouse_driver import Client
 import requests
 import json
-from multiprocessing import Process
+from multiprocessing import Pool
 import time
+import logging
 
 # Глобальные переменные
-api_dev_key = 'yAMu96g8htI5R4dDmLh6PfZmhyKRJd-Q'
-external_service_url = 'https://pastebin.com/api/api_post.php'
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
-clickhouse_client = Client(host='localhost', port=9000, user='default', password='123')
 
-def retry(func, max_retries=3, delay=2):
+logging.basicConfig(filename='srvs.log', filemode='a', level=logging.INFO)   # Логирование
+logger = logging.getLogger(__name__)
+
+api_dev_key = 'yAMu96g8htI5R4dDmLh6PfZmhyKRJd-Q'                  # Публикация
+external_service_url = 'https://pastebin.com/api/api_post.php'
+
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)                      
+clickhouse_client = Client(host='localhost', port=9000, user='default', password='123')  
+
+
+def retry(func, max_retries=3, delay=2):        # Функция обработки исключений и повтора
     for _ in range(max_retries):
         try:
             return func()
         except Exception as e:
-            print(f"Error: {e}")
+            print(f"Error: {e}, тип ошибки: {type(e).__name__}")
+            logger.error(f"Error: {e}, тип ошибки: {type(e).__name__}")
             time.sleep(delay)
-    raise RuntimeError("Достизнуто максимальное количество попыток")
+    raise RuntimeError("Достигнуто максимальное количество попыток")
 
 def create_pastebin_retry(api_dev_key, api_paste_code, api_paste_private, api_paste_name,
                     api_paste_expire_date, api_paste_format, api_user_key, api_option):
@@ -68,16 +76,13 @@ def search_and_send_retry(data):
 
     return retry(search_and_send)
 
-def process_queue():
-    while True:
-        queue_data = redis_client.blpop('search_queue')
-
-        if queue_data:
-            data = json.loads(queue_data[1])
-            p = Process(target=search_and_send_retry, args=(data,))
-            p.start()
+def process_queue_wrapper(queue_data):
+    data = json.loads(queue_data[1])
+    search_and_send_retry(data)
 
 if __name__ == '__main__':
-    for _ in range(5):
-        p = Process(target=process_queue)
-        p.start()
+    with Pool(1) as pool:  # Ограничение на 4 процесса
+        while True:
+            queue_data = redis_client.blpop('search_queue')
+            if queue_data:
+                pool.apply_async(process_queue_wrapper, (queue_data,))
